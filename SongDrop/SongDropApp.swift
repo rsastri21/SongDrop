4eeb7b8add82846464502d781673b41a6af16582
rsastri21 <rsastri21@gmail.com>
2026-03-02 21:41:20 -0800

    Initial Commit

---
+++ SongDrop/SongDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongDropApp.swift
+//  SongDrop
+//
+//  Created by Rohan Sastri on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SongDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SongDropTests/SongDropTests.swift
@@ -0,0 +1,17 @@
+//
+//  SongDropTests.swift
+//  SongDropTests
+//
+//  Created by Rohan Sastri on 3/2/26.
+//
+
+import Testing
+@testable import SongDrop
+
+struct SongDropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
